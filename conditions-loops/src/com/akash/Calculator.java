a4991deb7ce25346c662647ef786ae27a83e8da6
Akash Shukla <akashshukla1442@gmail.com>
2022-06-05 12:57:43 +0530

    Initial Commit

---
+++ conditions-loops/src/com/akash/Calculator.java
@@ -0,0 +1,42 @@
+package com.akash;
+import java.util.Scanner;
+public class Calculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //Take input from user till user does not print X or x
+        int ans = 0;
+        while(true){
+            //Take the operator as input
+            System.out.println("Enter the operator :");
+            char op = sc.next().trim().charAt(0);
+            if(op == '+' || op == '-' || op == '*'|| op == '/' || op == '%'){
+                //Input two numbers
+                System.out.println("Enter two numbers :");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+                if(op == '+'){
+                    ans = num1 + num2;
+                }
+                if(op == '-'){
+                    ans = num1 - num2;
+                }
+                if(op == '*'){
+                    ans = num1 * num2;
+                }
+                if(op == '/'){
+                    if(num2 != 0){
+                        ans = num1 / num2;
+                    }
+                }
+                if(op == '%'){
+                    ans = num1 % num2;
+                }
+            }else if(op == 'x' || op == 'X'){ //After entering x/X , program will terminate.
+                break;
+            }else{
+                System.out.println("Invalid Operation !"); //If we put any other character then it will shoe this message.
+            }
+            System.out.println(ans);
+        }
+    }
+}
